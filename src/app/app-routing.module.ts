5035af49d0024901e08bbf08a5b1c54dfa701b08
Dimitar Naydenov <naidim@abv.bg>
2020-04-20 01:30:16 +0300

    Added the info component, with temporary test routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './todo/list/list.component';
 import { DetailsComponent } from './todo/details/details.component';
+import { InfoComponent } from './todo/info/info.component';
 
 const routes: Routes = [
   { path: '', component: ListComponent },
-  { path: 'details', component: DetailsComponent }
+  { path: 'info', component: InfoComponent },
+  { path: 'details', component: DetailsComponent },
 ];
 
 @NgModule({
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
-  declarations: [ListComponent, DetailsComponent],
+  declarations: [ListComponent, DetailsComponent, InfoComponent],
   imports: [CommonModule],
 })
 export class TodoModule { }
