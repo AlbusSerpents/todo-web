fc347061b468215a50e7149d0a2f58a0e84674aa
Dimitar Naydenov <naidim@abv.bg>
2020-04-20 01:04:46 +0300

    created the TODO-s module and attached the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './todo/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent }
+];
 
 @NgModule({
   exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TodoModule } from './todo/todo.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TodoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
