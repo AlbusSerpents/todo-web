1530e9e958b128580eb9933e6eb2bd56fb6841bd
Dimitar Naydenov <naidim@abv.bg>
2020-05-02 19:00:39 +0300

    removed details component, to be merged with info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './todo/list/list.component';
-import { DetailsComponent } from './todo/details/details.component';
-import { InfoComponent } from './todo/info/info.component';
 
 const routes: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'info', component: InfoComponent },
-  { path: 'details', component: DetailsComponent },
+  { path: '', component: ListComponent }
 ];
 
 @NgModule({
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
-import { DetailsComponent } from './details/details.component';
 import { InfoComponent } from './info/info.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [ListComponent, DetailsComponent, InfoComponent],
+  declarations: [ListComponent, InfoComponent],
   imports: [CommonModule, FontAwesomeModule],
 })
 export class TodoModule { }
